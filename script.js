9752aa246d1cde7b0b4dc7eeb023468c28c3d66d
Bonderukk <matusmatonibednarik@gmail.com>
2025-02-02 21:23:57 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,4 +5,23 @@ window.addEventListener('scroll', function() {
     } else {
         header.classList.remove('scrolled');
     }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+    const body = document.querySelector("body");
+
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+        body.style.overflow = navMenu.classList.contains("active") ? "hidden" : "auto";
+    });
+
+    // Close menu when clicking a link
+    document.querySelectorAll(".nav-menu li a").forEach(n => n.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+        body.style.overflow = "auto";
+    }));
 }); 
